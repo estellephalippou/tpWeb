b2609be97fe77593b52ec1cddc110018bcb8bf90
estelle <estelle.phalippou@univ-rennes.fr>
2025-09-23 16:00:54 +0200

    Pariti controller à faire

--- interaction.js
+++ interaction.js
@@ -2,11 +2,43 @@
 // La création d'un Dnd requière un canvas et un interacteur.
 // L'interacteur viendra dans un second temps donc ne vous en souciez pas au départ.
 function DnD(canvas, interactor) {
-	// Définir ici les attributs de la 'classe'
+  this.coordonneesInit = { x: 0, y: 0 };
+  this.coordonneesFinal = { x: 0, y: 0 };
 
-	// Developper les 3 fonctions gérant les événements
+ 
+  this.isDragged = false;
 
-	// Associer les fonctions précédentes aux évènements du canvas.
+ this.pression = function (evt) {
+    this.coordonneesInit = getMousePosition(canvas, evt);
+    this.isDragged = true;
+    if (interactor?.onInteractionStart) {
+      interactor.onInteractionStart(this);
+    }
+    console.log("pression: "  + this.coordonneesFinal.x +" "+this.coordonneesFinal.y);
+  }.bind(this);
+
+  this.deplacement = function (evt) {
+    if (!this.isDragged) return;
+    this.coordonneesFinal = getMousePosition(canvas, evt);
+    if (interactor?.onInteractionUpdate) {
+      interactor.onInteractionUpdate(this);
+    }
+    console.log("deplacement: " + this.coordonneesFinal.x +" "+this.coordonneesFinal.y);
+  }.bind(this);
+
+  this.relachement = function (evt) {
+    if (!this.isDragged) return;
+    this.coordonneesFinal = getMousePosition(canvas, evt);
+    this.isDragged = false;
+    if (interactor?.onInteractionEnd) {
+      interactor.onInteractionEnd(this);
+    }
+    console.log("relachement: " + this.coordonneesFinal.x +" "+this.coordonneesFinal.y);
+  }.bind(this);
+
+  canvas.addEventListener('mousedown', this.pression);
+  canvas.addEventListener('mousemove', this.deplacement);
+  canvas.addEventListener('mouseup', this.relachement);
 };
 
 
@@ -18,6 +50,4 @@ function getMousePosition(canvas, evt) {
     y: evt.clientY - rect.top
   };
 };
-
-
 
--- model.js
+++ model.js
@@ -1,3 +1,59 @@
 
 // Implémenter ici les 4 classes du modèle.
 // N'oubliez pas l'héritage !
+
+class Drawing {
+  constructor() {
+    this.forms = [];
+  }
+  paint(ctx,canvas) {
+    ctx.fillStyle = '#F0F0F0'; // set canvas' background color
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    this.getForms().forEach(function (eltDuTableau) {
+      // now fill the canvas
+      eltDuTableau.paint(ctx);
+
+    })
+  }
+
+}
+
+// Classe Rectangle
+class Rectangle extends Drawing {
+  constructor(x, y, width, height, thickness, color) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.thickness = thickness;
+    this.color = color;
+  }
+
+  paint(ctx) {
+
+    ctx.rect(this.x, this.y, this.width, this.height);
+    ctx.stroke();
+  }
+}
+
+
+
+// Classe Line
+class Line extends Drawing {
+  constructor(x1, y1, x2, y2, thickness, color) {
+    this.x1 = x1;
+    this.y1 = y1;
+    this.x2 = x2;
+    this.y2 = y2;
+    this.thickness = thickness;
+    this.color = color;
+  }
+
+  paint(ctx) {
+    ctx.moveTo(this.x1, this.y1);
+    ctx.lineTo(this.x2, this.y2);
+    ctx.stroke();
+  }
+}
+
+
